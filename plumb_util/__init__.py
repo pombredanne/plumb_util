95ec0db7ae99fd4b481d9e15f45e5836cd1bd665
Jason Bernardino Alonso <jalonso@lumino.so>
2012-03-05 17:42:11 -0500

    Implemented a straightforward, credservice-compatible HMAC tool

--- plumb_util/__init__.py
+++ plumb_util/__init__.py
@@ -1,2 +1,4 @@
 
 from .dns_srv import find_service
+from .auto_hmac import hmac_wrap, assert_hmac, lookup_from_secrets,
+signers_from_secrets
---
+++ plumb_util/auto_hmac.py
@@ -0,0 +1,51 @@
+
+from hmac import new as HMAC
+import hashlib
+from base64 import b64decode
+from time import time as epoch
+
+def hmac_from_name(alg_name='sha256'):
+    assert alg_name in hashlib.algorithms, "Hash algorithm %s not known" % alg_name
+    alg_constructor = getattr(hashlib, alg_name)
+    return lambda key, msg=None: HMAC(key, msg, alg_constructor)
+
+def _hmac(msg, alg_name, key):
+    hm = hmac_from_name(alg_name)
+    return hm(key, msg).hexdigest()
+
+def hmac_wrap(msg, secret):
+    assert not secret['accept_only'], "Key not valid for signing"
+    handle, alg_name, key = [secret[k] for k in ('name', 'type', 'key')]
+    timestamp = int(epoch())
+    hm_digest = _hmac(handle +'\n' + str(timestamp) + '\n' + msg, alg_name, key)
+    return {'msg': msg, 'hmac': hm_digest, 'handle': handle, 'timestamp': timestamp}
+
+def lookup_from_secrets(secrets):
+    lookup = {}
+    for secret in secrets:
+        secret = secret.copy()
+        secret['key'] = b64decode(secret['key'])
+        lookup[secret['name']] = secret
+    return lookup
+
+def signers_from_secrets(secrets):
+    signers = []
+    for secret in secrets:
+        secret = secret.copy()
+        secret['key'] = b64decode(secret['key'])
+        if not secret['accept_only']:
+            signers.append(secret)
+    return signers
+
+def assert_hmac(msg, lookup):
+    assert 'hmac' in msg, "hmac field missing"
+    assert 'msg' in msg, "msg field missing"
+    assert 'handle' in msg, "handle field missing"
+    assert 'timestamp' in msg, "timestamp field missing"
+    handle = msg['handle']
+    assert handle in lookup, "Handle unknown"
+    alg_name, key, max_age_secs = [lookup[handle][k] for k in ('type', 'key', 'max_age_secs')]
+    assert epoch() < msg['timestamp'] + max_age_secs, "Message expired"
+    hm_digest = _hmac(handle + '\n' + str(msg['timestamp']) + '\n' + msg['msg'], alg_name, key)
+    assert hm_digest == msg['hmac'], "Signature invalid"
+    return True
